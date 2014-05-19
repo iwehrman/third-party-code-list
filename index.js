3183cf07146a79aa52dd632d7fec2f747b1ec601
Ian Wehrman <wehrman@adobe.com>
2014-05-19 13:34:41 -0700

    Show all licenses; show version number

--- index.js
+++ index.js
@@ -25,18 +25,30 @@
                 return;
             }
 
+            var getLicenseMarkup = function (license) {
+                if (typeof license === "object" && license.hasOwnProperty("type")) {
+                    return license.url ? util.format("[%s](%s)", license.type, license.url) : license.type;
+                } else {
+                    return license;
+                }
+            };
+
+            var getLicensesMarkup = function (pkg) {
+                if (pkg.licenses) {
+                    return pkg.licenses.map(getLicenseMarkup).join(", ");
+                } else if (pkg.license) {
+                    return getLicenseMarkup(pkg.license);
+                }
+            };
+
             var getMarkup = function (pkg, parsed) {
                 parsed = parsed || {};
 
                 var homepage = pkg.homepage || pkg.repository,
                     nameMarkup = homepage ? util.format("[%s](%s)", pkg.name, homepage) : pkg.name,
-                    license = (typeof pkg.license === "object" && pkg.license.hasOwnProperty("type")) ?
-                        (pkg.license.hasOwnProperty("url") ?
-                            util.format("[%s](%s)", pkg.license.type, pkg.license.url) :
-                            pkg.license.type) :
-                        pkg.license,
-                    licenseMarkup = license || "Unknown",
-                    markup = " * " + nameMarkup + " - " + licenseMarkup;
+                    licenseMarkup = getLicensesMarkup(pkg) || "Unknown",
+                    versionMarkup = pkg.version ? "@" + pkg.version : "",
+                    markup = " * " + nameMarkup + versionMarkup + " - " + licenseMarkup;
 
                 parsed[pkg.name] = markup;
 
